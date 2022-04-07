ffa23a8078f6cf56c8b372642b295be63790583d
RafaelStudartDiPiero <rafael.spiero@gmail.com>
2022-04-07 18:33:18 -0300

    Adding MultiPlayer and Clarifications in Game.py

--- game.py
+++ game.py
@@ -22,10 +22,11 @@ class Game:
         # Game States
         self.is_running = True
         self.state = 'menu'
+        self.multiplayer = False
         # Starting Menu Options
         self.menu_options = 0
         # Defining BackGrounds
-        self.single_background = None
+        self.playing_background = None
         self.menu_background = None
         self.gameover_background = None
         # Audio Directory
@@ -88,16 +89,16 @@ class Game:
         self.collisions_bomb_player2 = None
 
     def run(self):
-        """"""
+        """Defines the Game State and calls its corresponding methods."""
         while self.is_running:
             if self.state == 'menu':
                 self.menu_events()
                 self.menu_update()
                 self.menu_draw()
-            if self.state == 'singleplayer':
-                self.single_events()
-                self.single_update()
-                self.single_draw()
+            if self.state == 'playing':
+                self.playing_events()
+                self.playing_update()
+                self.playing_draw()
             if self.state == 'gameover':
                 self.gameover_events()
                 self.gameover_update()
@@ -109,18 +110,22 @@ class Game:
 # ---------------------------- GENERAL FUNCTIONS -----------------------------------------
 
     def load_files(self):
-        """"""
+        """Loads important images, audios and creates the list with background walls"""
+        # Getting the Image Directory
         dir_images = os.path.join(os.getcwd(), 'images')
+        # Getting the Audio Directory
         self.dir_audios = os.path.join(os.getcwd(), 'audios')
+        # Loading the Menu Background
         self.menu_background = os.path.join(dir_images, MENU_BACKGROUND)
         self.menu_background = pygame.image.load(self.menu_background).convert()
         self.menu_background = pygame.transform.scale(self.menu_background,
                                                       (WIDTH, HEIGHT))
-        self.single_background = os.path.join(dir_images, SINGLE_BACKGROUND)
-        self.single_background = pygame.image.load(self.single_background).convert()
-        self.single_background = pygame.transform.scale(self.single_background,
-                                                        (WIDTH, HEIGHT))
-
+        # Loading the Playing Background
+        self.playing_background = os.path.join(dir_images, SINGLE_BACKGROUND)
+        self.playing_background = pygame.image.load(self.playing_background).convert()
+        self.playing_background = pygame.transform.scale(self.playing_background,
+                                                         (WIDTH, HEIGHT))
+        # Loading the Game Over Background
         self.gameover_background = os.path.join(dir_images, GAME_OVER_BACKGROUND)
         self.gameover_background = pygame.image.load(self.gameover_background).convert()
         self.gameover_background = pygame.transform.scale(self.gameover_background,
@@ -133,7 +138,7 @@ class Game:
                         self.walls.append(vector(x, y))
 
     def write_text(self, window, size, color, font_name, msg, position):
-        """"""
+        """Generic method that writes messages in the window"""
         font = pygame.font.SysFont(font_name, size)
         text = font.render(msg, False, color)
         text_size = text.get_size()
@@ -142,6 +147,7 @@ class Game:
         window.blit(text, position)
 
     def draw_grid(self):
+        """Method to draw the grid in the map"""
         for x in range(X_CELLS):
             pygame.draw.line(self.window, GREY, (x*self.cell_width, 0),
                              (x*self.cell_width, HEIGHT))
@@ -152,7 +158,7 @@ class Game:
 # ----------------------------- INTRO FUNCTIONS ------------------------------------------
 
     def menu_events(self):
-        """"""
+        """Defines events that can happen in the menu"""
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 self.is_running = False
@@ -167,28 +173,32 @@ class Game:
                         self.menu_options = 0
                 if event.key == pygame.K_SPACE:
                     if self.menu_options == 0:
-                        self.state = 'singleplayer'
+                        self.state = 'playing'
                     elif self.menu_options == 1:
-                        self.state = 'multiplayer'
+                        self.state = 'playing'
+                        self.multiplayer = True
                     elif self.menu_options == 2:
                         self.state = 'challenge'
 
     def menu_update(self):
-        """"""
+        """Method that updates the state of the menu"""
         pass
 
     def menu_draw(self):
-        """"""
+        """Draws elements that make the menu"""
         single_color = BLACK
         multi_color = BLACK
         challenge_color = BLACK
+        # Changes color of selected option
         if self.menu_options == 0:
             single_color = WHITE
         elif self.menu_options == 1:
             multi_color = WHITE
         elif self.menu_options == 2:
             challenge_color = WHITE
+        # Puts the background image
         self.window.blit(self.menu_background, (0, 0))
+        # Writes the Options
         self.write_text(self.window, MENU_TEXT_SIZE, single_color,
                         MENU_TEXT_FONT, 'Single Player', [150, 230])
         self.write_text(self.window, MENU_TEXT_SIZE, multi_color,
@@ -197,18 +207,22 @@ class Game:
                         MENU_TEXT_FONT, 'Challenge', [150, 330])
         pygame.display.update()
 
-# ------------------------- SINGLEPLAYER FUNCTIONS ---------------------------------------
+# ------------------------- Playing FUNCTIONS ---------------------------------------
 
-    def single_events(self):
-        """"""
+    def playing_events(self):
+        """Defines events that can happen during the playing"""
+
         for event in pygame.event.get():
+            # Quit Option
             if event.type == pygame.QUIT:
                 self.is_running = False
+            # Defines Player 1 Drop Bomb Key
             if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
                 self.player1.drop_bomb()
+            # Defines Player 2 Drop Bomb Key
             if event.type == pygame.KEYDOWN and event.key == pygame.K_m:
                 self.player2.drop_bomb()
-
+        # Defines Player 1 Movement
         if pygame.key.get_pressed()[pygame.K_w]:
             self.player1.move(vector(0, -1), 0)
         elif pygame.key.get_pressed()[pygame.K_s]:
@@ -219,7 +233,7 @@ class Game:
             self.player1.move(vector(1, 0), 1)
         else:
             self.player1.move(vector(0, 0), 0)
-
+        # Defines Player 2 Movement
         if pygame.key.get_pressed()[pygame.K_UP]:
             self.player2.move(vector(0, -1), 0)
         elif pygame.key.get_pressed()[pygame.K_DOWN]:
@@ -231,6 +245,12 @@ class Game:
         else:
             self.player2.move(vector(0, 0), 0)
 
+        # Checks if Multiplayer was Chosen:
+        if not self.multiplayer:
+            if not self.player2.destroyed:
+                self.player2.destroy()
+
+        # Checks if Player1 has a banned direction and must be blocked
         self.is_player1_blocked = False
         for wall in self.walls:
             dist_x = abs((wall.x + 1/2) * self.cell_width - self.player1.pix_pos.x)
@@ -245,96 +265,144 @@ class Game:
             self.player1.blocked = False
             self.player1.banned_direction = vector(1, 1)
 
-        self.is_player2_blocked = False
-        for wall in self.walls:
-            dist_x = abs((wall.x + 1 / 2) * self.cell_width - self.player2.pix_pos.x)
-            dist_y = abs((wall.y + 1 / 2) * self.cell_height - (self.player2.pix_pos.y + 20))
+        # Checks if Player2 has a banned direction and must be blocked, if it is not destroyed
+        if not self.player2.destroyed:
+            self.is_player2_blocked = False
+            for wall in self.walls:
+                dist_x = abs((wall.x + 1 / 2) * self.cell_width - self.player2.pix_pos.x)
+                dist_y = abs((wall.y + 1 / 2) * self.cell_height - (self.player2.pix_pos.y + 20))
 
-            if dist_x < self.cell_width * 3 / 5 and dist_y < self.cell_height * 3 / 5:
-                self.player2.ban_direction()
-                self.player2.blocked = True
-                self.is_player2_blocked = True
+                if dist_x < self.cell_width * 3 / 5 and dist_y < self.cell_height * 3 / 5:
+                    self.player2.ban_direction()
+                    self.player2.blocked = True
+                    self.is_player2_blocked = True
 
-        if not self.is_player2_blocked:
-            self.player2.blocked = False
-            self.player2.banned_direction = vector(1, 1)
+            if not self.is_player2_blocked:
+                self.player2.blocked = False
+                self.player2.banned_direction = vector(1, 1)
 
-    def single_update(self):
-        if self.collisions1 or self.collisions2:
-            self.state = 'gameover'
+    def playing_update(self):
+        """Updates and Checks Conditions of Playing Game State"""
+        # Checks if one of the player has died by a collision.
+        if (not self.player1.destroyed and self.collisions1) or (not self.player2.destroyed and self.collisions2):
+            if self.collisions1:
+                self.all_sprites.remove(self.player1)
+                self.player1.destroy()
+                # If player 1 is destroyed and player 2 is already destroyed, the game ends.
+                if self.player2.destroyed:
+                    self.state = 'gameover'
+            else:
+                self.all_sprites.remove(self.player2)
+                self.player2.destroy()
+                # If player 2 is destroyed and player 1 is already destroyed, the game ends.
+                if self.player1.destroyed:
+                    self.state = 'gameover'
         else:
+            # Updates Enemy,Blocks and Players
             self.enemy.update()
             for i in range(len(self.blocks)):
                 self.blocks[i].update()
             self.player1.update()
             self.player2.update()
+        # Checks if the enemy has collided with the bomb of player 1 or 2
         if self.collisions_bomb1 or self.collisions_bomb2:
             self.all_sprites.remove(self.enemy)
             self.enemy.destroy()
+        # Checks if the player1 has collided with the bomb of player 1 or 2
         if self.collisions_bomb_player1:
             self.all_sprites.remove(self.player1)
             self.player1.destroy()
+            # If player 1 is destroyed and player 2 is already destroyed, the game ends.
+            if self.player2.destroyed:
+                self.state = 'gameover'
+        # Checks if the player2 has collided with the bomb of player 1 or 2
         if self.collisions_bomb_player2:
             self.all_sprites.remove(self.player2)
             self.player2.destroy()
+            # If player 2 is destroyed and player 1 is already destroyed, the game ends.
+            if self.player1.destroyed:
+                self.state = 'gameover'
 
+        # Checks collisions with Player1.
         self.collisions1 = pygame.sprite.spritecollide(self.player1, self.collision_sprites, False
                                                        , pygame.sprite.collide_mask)
+        # Checks multiple collisions with Player1 Bomb when bomb is exploded.
         if self.player1.bomb.exploded:
+            # Checks collisions between Enemy and Bomb1.
             self.collisions_bomb1 = pygame.sprite.spritecollide(self.player1.bomb, self.collision_sprites_bomb
                                                                 , False, pygame.sprite.collide_mask)
+            # Checks collisions between Player1 and Bomb1.
+            # @TODO Bug Bomb1 explodes and player in contact with Bomb2 dies
             self.collisions_bomb_player1 = pygame.sprite.spritecollide(self.player1, self.collision_sprites_bomb_player
                                                                        , False, pygame.sprite.collide_mask)
+            # Checks collisions between Player2 and Bomb1.
+            # @TODO Bug Bomb1 explodes and player in contact with Bomb2 dies
             self.collisions_bomb_player2 = pygame.sprite.spritecollide(self.player2, self.collision_sprites_bomb_player
                                                                        , False, pygame.sprite.collide_mask)
-        self.collisions_bomb_block1 = pygame.sprite.spritecollide(self.player1.bomb, self.collision_sprites_bomb_block
-                                                                  , False, pygame.sprite.collide_mask)
+            # Checks collisions between Blocks and Bomb1.
+            self.collisions_bomb_block1 = pygame.sprite.spritecollide(self.player1.bomb,
+                                                                      self.collision_sprites_bomb_block
+                                                                      , False, pygame.sprite.collide_mask)
 
+        # Checks collisions with Player2.
         self.collisions2 = pygame.sprite.spritecollide(self.player2, self.collision_sprites, False
                                                        , pygame.sprite.collide_mask)
+        # Checks multiple collisions with Player2 Bomb.
         if self.player2.bomb.exploded:
+            # Checks collisions between Enemy and Bomb2.
             self.collisions_bomb2 = pygame.sprite.spritecollide(self.player2.bomb, self.collision_sprites_bomb
                                                                 , False, pygame.sprite.collide_mask)
+            # Checks collisions between Player1 and Bomb2.
+            # @TODO Bug Bomb2 explodes and player in contact with Bomb1 dies
             self.collisions_bomb_player1 = pygame.sprite.spritecollide(self.player1, self.collision_sprites_bomb_player
                                                                        , False, pygame.sprite.collide_mask)
+            # Checks collisions between Player2 and Bomb2.
+            # @TODO Bug Bomb2 explodes and player in contact with Bomb1 dies
             self.collisions_bomb_player2 = pygame.sprite.spritecollide(self.player2, self.collision_sprites_bomb_player
                                                                        , False, pygame.sprite.collide_mask)
-        self.collisions_bomb_block2 = pygame.sprite.spritecollide(self.player2.bomb, self.collision_sprites_bomb_block,
-                                                                  False, pygame.sprite.collide_mask)
+            # Checks collisions between Blocks and Bomb2.
+            self.collisions_bomb_block2 = pygame.sprite.spritecollide(self.player2.bomb,
+                                                                      self.collision_sprites_bomb_block,
+                                                                      False, pygame.sprite.collide_mask)
         # print(self.collisions1)
+        # print(self.collisions2)
         # print(self.collisions_bomb1)
         # print(self.collisions_bomb_block1)
-        # print(self.clock.get_fps())
+        print(self.clock.get_fps())
 
-    def single_draw(self):
-        """"""
-        self.window.blit(self.single_background, (0, 0))
+    def playing_draw(self):
+        """Drawing elements and sprites"""
+        # Draws background
+        self.window.blit(self.playing_background, (0, 0))
         self.draw_grid()
+        # Draws Player Elements(Bomb)
         self.player1.draw()
         self.player2.draw()
+        # Draws All Sprites used
         self.all_sprites.draw(self.window)
         pygame.display.update()
 
 # ---------------------------- GAME OVER FUNCTIONS ---------------------------------------
 
     def gameover_events(self):
-        """"""
+        """Defines events that happens during Gameover"""
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 self.is_running = False
             if event.type == pygame.KEYDOWN:
-                self.state = 'menu'
                 self.is_running = False
 
         pass
 
     def gameover_update(self):
-        """"""
+        """Updates elements in GameOver State"""
         pass
 
     def gameover_draw(self):
-        """"""
+        """Draws Elements in the Window"""
+        # Draws Background
         self.window.blit(self.gameover_background, (0, 0))
+        # Write GameOver text
         self.write_text(self.window, MENU_TEXT_SIZE, WHITE,
                         MENU_TEXT_FONT, 'PRESS ANY KEY TO QUIT', [400, 170])
         pygame.display.update()
